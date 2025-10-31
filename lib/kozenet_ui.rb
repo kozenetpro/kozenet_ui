0c05ea8d435aa0e24c59a4f68d0cb147a858f9c7
rubemoon <aprovadosatisfy@gmail.com>
2025-10-31 16:19:33 -0300

    Initialize Kozenet UI gem with essential files and configurations

---
+++ lib/kozenet_ui.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "kozenet_ui/version"
+
+module KozenetUi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kozenet_ui/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module KozenetUi
+  VERSION = "0.1.0"
+end
